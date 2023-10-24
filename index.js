84bd2bb965ef730b14919a00dfa09eeb08f3c237
Tony Eder <tonyeder11@gmail.com>
2023-10-23 19:04:34 -0700

    trying to get changes to resolve on frontend

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-let form = document.querySelector('form').addEventListener('submit', (e) => {
-    console.log(e);
+let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target['new-task-description'].value)
+    form.reset()
 })
+
+function buildToDo(todo){
+    let li = document.createElement('li')
+    li.textContent = `${todo} `
+    document.querySelector('#tasks').appendChild(li)
+    let btn = document.createElement('button')
+    li.appendChild(btn)
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = " x "
+    form.reset();
+
+}
+
+function handleDelete(e){
+    e.target.parentNode.remove()
+}
+
+//UPdate h1 to h2
+let itBeH2 = document.querySelector('h1')
+itBeH2.outerHTML = '<h2>' + itBeH2.innerHTML + '</h2>'
+
+//add button class and style - style on css sheet
+document.querySelectorAll('input')[1].className = "button"
+
